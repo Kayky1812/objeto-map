53ea9a46f9045b00c25a4209c334dde95f8f5860
Kayky Santos Da Cunha Couto <134162809+Kayky1812@users.noreply.github.com>
2024-01-27 18:05:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const pessoas = [
+    {id:3,nome:'Luiz'},
+    {id:2,nome: 'Maria'},
+    {id:1,nome: 'Kayky'},
+];
+
+const novasPessoas = new Map();
+for (const pessoa of pessoas){
+    const {id}= pessoa;
+    novasPessoas.set(id,{...pessoa});
+}
+
+for (const pessoas of novasPessoas){
+    console.log(pessoas);
+}
+
